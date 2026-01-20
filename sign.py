db876150e6a703bd7d69f76ace976f056232e942
Lincoln-sensei <lnclnngugi@gmail.com>
2026-01-20 08:47:02 +0300

    Implement signing function using Ed25519

--- sign.py
+++ sign.py
@@ -1,1 +1,8 @@
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+from pathlib import Path
 
+PRIVATE_KEY_FILE = Path(".keys/private.key")
+
+def sign(payload: bytes) -> bytes:
+    private_key = Ed25519PrivateKey.from_private_bytes(PRIVATE_KEY_FILE.read_bytes())
+    return private_key.sign(payload)
